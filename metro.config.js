a55702966bbb3c5916d1347492398fb65cf90b74
Junho Yeo <i@junho.io>
2022-04-26 00:37:36 +0900

    Install dependencies for WalletConnect

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,17 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const crypto = require.resolve('crypto-browserify');
 
 module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
+  resolver: {
+    extraNodeModules: {
+      crypto,
+      fs: require.resolve('expo-file-system'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      net: require.resolve('react-native-tcp'),
+      os: require.resolve('os-browserify/browser.js'),
+      path: require.resolve('path-browserify'),
+      stream: require.resolve('readable-stream'),
+      vm: require.resolve('vm-browserify'),
+    },
   },
 };
