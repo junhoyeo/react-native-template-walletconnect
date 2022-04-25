9c02c9cf3f6a4ff6e7ed59d0db316777bed7ab52
Junho Yeo <i@junho.io>
2022-04-26 01:53:47 +0900

    Ignore yellow boxes

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 // prettier-ignore
 import './shim';
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 
 import App from './App';
 import { name as appName } from './app.json';
+
+LogBox.ignoreLogs([
+  // FIXME: Task orphaned for request warnings
+  'Task orphaned for request',
+
+  // https://github.com/tradle/react-native-crypto/issues/30
+  'Require cycle',
+]);
 
 AppRegistry.registerComponent(appName, () => App);
